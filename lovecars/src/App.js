16296addad3ecb3eda8fdd01a59e01ef1935a145
KarthikPaidipalli <paidipallikarthik1510@gmail.com>
2024-02-25 19:40:18 +0530

    inthirdfour

--- lovecars/src/App.js
+++ lovecars/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
-
-import Header from "./components/Header"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <Header/>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
+
--- lovecars/src/components/Header/index.js
+++ lovecars/src/components/Header/index.js
@@ -1,15 +1,16 @@
 
-import "./index.css"
+import "./index.css";
 import { FaCar } from "react-icons/fa";
 
-const Header =()=>{
-    return(
+const Header = () => {
+    return (
         <div className="headercontainer">
-            <FaCar size={25}/>
+            <FaCar size={25} />
             <p>Home</p>
             <p>Description</p>
-            <a href="https://www.automotiveworld.com/articles/" target="_blank" className="art">Articles</a>
+            <a href="https://www.automotiveworld.com/articles/" target="_blank" rel="noopener noreferrer" className="art">Articles</a>
         </div>
-    )
-}
-export default Header
+    );
+};
+
+export default Header;
