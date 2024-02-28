1214c4528213ebb91abe06c0f0f88bf673780826
KarthikPaidipalli <paidipallikarthik1510@gmail.com>
2024-02-28 21:43:20 +0530

    thirddid

--- lovecars/src/components/Header/index.js
+++ lovecars/src/components/Header/index.js
@@ -1,14 +1,13 @@
 
 import "./index.css";
 import { FaCar } from "react-icons/fa";
-
 const Header = () => {
     return (
         <div className="headercontainer">
             <FaCar size={25} />
             <p>Home</p>
             <p>Description</p>
-            <a href="https://www.automotiveworld.com/articles/" target="_blank" rel="noopener noreferrer" className="art">Articles</a>
+            <a href="https://www.automotiveworld.com/articles/" target="_blank" rel="noopener noreferrer" className="underscore">Articles</a>
         </div>
     );
 };
