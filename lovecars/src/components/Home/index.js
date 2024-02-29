c4aca1f1eef2ed219c3cce4b5aeefcf1a31f24c9
KarthikPaidipalli <paidipallikarthik1510@gmail.com>
2024-02-29 22:14:42 +0530

    Addedbackgroundimages

--- lovecars/src/components/Home/index.js
+++ lovecars/src/components/Home/index.js
@@ -1,11 +1,14 @@
 import Header from "../Header";
-
+import "./index.css"
 const Home = ()=>{
   
     return(
-        <div>
+        <div className="hometotalcontainer">
             <Header/>
-
+            <div className="homecontainer">
+               
+                <h1>akjgc</h1>
+            </div>
         </div>
     )
 
