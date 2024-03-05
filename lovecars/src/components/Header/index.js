7e93948c29e40c2e2b6d65dfa94cb80718cb5727
KarthikPaidipalli <paidipallikarthik1510@gmail.com>
2024-03-06 04:14:51 +0530

    addedlink

--- lovecars/src/components/Header/index.js
+++ lovecars/src/components/Header/index.js
@@ -1,11 +1,12 @@
 
 import "./index.css";
 import { FaCar } from "react-icons/fa";
+import {Link} from "react-router-dom";
 const Header = () => {
     return (
         <div className="headercontainer">
             <FaCar size={25} />
-            <p>Home</p>
+            <Link to="/"><p>Home</p></Link>
             <p>Description</p>
             <a href="https://www.automotiveworld.com/articles/" target="_blank" rel="noopener noreferrer" className="underscore">Articles</a>
         </div>
